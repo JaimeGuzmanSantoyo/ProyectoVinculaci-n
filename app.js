404b4fb07a139168665f2aaf313e1f178c27729f
LuisRodrigoNR <luisrnr2910@gmail.com>
2025-05-20 12:26:18 -0600

    rail

--- app.js
+++ app.js
@@ -2,75 +2,64 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 
-/*instanciar*/ 
 const app = express();
-app.use(express.static(__dirname + '/views')); // nos va servir para poder cargar  archivos estáticos desde la carpeta 'views'
-app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(__dirname + '/views')); // para archivos estáticos
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 
-//creedenciales para DB
+// Credenciales para DB, aquí idealmente usa variables de entorno (más abajo te explico)
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'profesores',
-    port: '3306'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '1234',
+    database: process.env.DB_NAME || 'profesores',
+    port: process.env.DB_PORT || '3306'
 });
 
-//conexion a la DB
-db.connect(err=>{
-    if(err){
-        console.log(`Error al momento de hacer conexion BB :3 ${err}`);
-    }else{
-        console.log(`Conexion realizada :3`);
+// Conexion a la DB
+db.connect(err => {
+    if (err) {
+        console.log(`Error al conectar a la base de datos: ${err}`);
+    } else {
+        console.log(`Conexión a la base de datos exitosa`);
     }
 });
-/*Puerto*/
-const port = 3038; 
-const hostName= 'localhost';
-//server inicio
-app.listen(port,hostName,()=>{
-    console.log(`El server esta en escucha desde http://${hostName}:${port}`);
 
+// Puerto dinámico para Railway, o 3038 para local
+const port = process.env.PORT || 3038;
 
-
+// No necesitas pasar hostname, para que escuche en todas las interfaces
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
 const path = require('path');
 
-
-
 app.get('/unam.jpg', (req, res) => {
-    res.sendFile(__dirname + '/views/unam.jpg');
-  });
-app.get('/', (req, res) => {
-   res.render('index');
+    res.sendFile(path.join(__dirname, 'views', 'unam.jpg'));
 });
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.post('/rename', (req, res) => {
-   res.render('rename');
+    res.render('rename');
 });
-
 
 app.get('/rename', (req, res) => {
-   res.render('rename');
+    res.render('rename');
 });
 
-
-//editar usuario
 app.post('/edit', (req, res) => {
-  res.render('edit'); 
+    res.render('edit');
 });
 
-//Ir a login de administrador
- app.get('/login_admin',(req, res)=>{
-  res.render('login_admin');
- })
-
- app.get('/grafica', (req, res) => {
-  res.render('grafica'); 
+app.get('/login_admin', (req, res) => {
+    res.render('login_admin');
 });
 
-
+app.get('/grafica', (req, res) => {
+    res.render('grafica');
+});
